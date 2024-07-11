24899fe3ca211663a64e2820829309c0bfa3e703
joshtin2505 <justincastro2505@gmail.com>
2024-07-10 22:26:36 -0500

    chore: Add lazy loading for images in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Nunito } from "next/font/google"
 import "./globals.css"
+import Providers from "./Providers"
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -18,13 +19,14 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <script src="/theme-script.js" async></script>
-
         <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
       </head>
-      <body className={`${nunito.className} antialiased`}>{children}</body>
+      <body className={`${nunito.className} antialiased`}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
