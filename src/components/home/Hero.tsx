3cb24cfa972e49a8dac12006c1eb3c63aeb7fe54
joshtin2505 <justincastro2505@gmail.com>
2024-07-10 21:08:17 -0500

    chore: Refactor Hero component layout and add reader count and newsletter footer

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -2,11 +2,12 @@ import React from "react"
 import { Button } from "../ui/button"
 import { Input } from "../ui/input"
 import Image from "next/image"
+import { FaNewspaper, FaStar } from "react-icons/fa6"
 
 function Hero() {
   return (
-    <section className="heroContainer overflow-hidden z-10">
-      <header className="grid col-span-5 gap-4">
+    <section className="heroContainer  z-10">
+      <header className="grid col-start-1 col-end-6 gap-4">
         <h1 className="text-5xl ">
           Stay up to date with the latest news and trends in Information
           Technology!
@@ -23,14 +24,29 @@ function Hero() {
           />
           <Button type="submit">Suscribe</Button>
         </div>
+        <footer className="flex items-center">
+          <div className="flex text-primary/50 items-center font-light pr-[15px] border-r border-primary/50">
+            <FaStar className="text-primary/70 mr-2" />
+            <span className="text-primary mr-2 grid place-items-center pt-0.5 font-normal">
+              1,456+
+            </span>
+            readers
+          </div>
+          <div className="pl-4 flex text-primary/80 items-center">
+            <FaNewspaper className="text-primary/70 mr-2" />
+            Join the Newsletter now!
+          </div>
+        </footer>
       </header>
-      <Image
-        className="col-start-7 col-end-12 border border-primary rounded aspect-[3/2] w-[800px] md:block hidden layerShadow"
-        src="/imgHero.jpg"
-        alt="Hero Image"
-        width={1000}
-        height={1000}
-      />
+      <figure className="col-start-8 col-end-12 md:grid hidden place-items-center h-full">
+        <Image
+          className=" border border-primary rounded aspect-[3/2] w-[800px]  layerShadow"
+          src="/imgHero.jpg"
+          alt="Hero Image"
+          width={1000}
+          height={1000}
+        />
+      </figure>
     </section>
   )
 }
