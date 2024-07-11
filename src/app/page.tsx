e2570228b230f506ef12aa231f535d36139da5d1
joshtin2505 <justincastro2505@gmail.com>
2024-07-10 23:46:19 -0500

    feat: Add Categories component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/sections/home/Hero"
 import NavLayout from "@/components/layouts/NavLayout"
+import Categories from "@/sections/home/Categories"
 
 export default function Home() {
   return (
     <NavLayout>
       <Hero />
+      <Categories />
     </NavLayout>
   )
 }
