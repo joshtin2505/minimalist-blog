c22a8b6e51224e4b532aad33b56a72bf130777d6
joshtin2505 <justincastro2505@gmail.com>
2024-07-10 23:59:50 -0500

    refactor: Update Categories component styling and image handling

--- src/sections/home/Categories.tsx
+++ src/sections/home/Categories.tsx
@@ -11,10 +11,10 @@ function Categories() {
         {categories.map((category) => {
           const { id, name } = category
           return (
-            <TabsContent value={id} key={id} className="pb-4 pt-2">
+            <TabsContent value={id} key={id} className="pb-4 pt-2 h-[94%]">
               {/* <h3 className="text-3xl">{name}</h3> */}
               <Image
-                className="w-full object-cover h-full aspect-[16/7] rounded"
+                className="w-full object-cover h-full aspect-auto rounded"
                 src={category.imageSrc}
                 alt={name}
                 width={2000}
