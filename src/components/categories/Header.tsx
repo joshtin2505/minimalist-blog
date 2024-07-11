1c21638f796cc5f44f632615b0bebd9164e51b9e
joshtin2505 <justincastro2505@gmail.com>
2024-07-10 23:46:39 -0500

    feat: Add Categories header component for displaying category tabs

---
+++ src/components/categories/Header.tsx
@@ -0,0 +1,24 @@
+import { CategoriesType } from "@/types"
+import { TabsList, TabsTrigger } from "../ui/tabs"
+
+function Header({ categories }: { categories: CategoriesType }) {
+  return (
+    <header className="flex w-full justify-between">
+      <h3 className="text-3xl">Categories</h3>
+
+      <TabsList className="">
+        {categories.map((category) => {
+          const { id, name } = category
+
+          return (
+            <TabsTrigger value={id} key={id}>
+              {name}
+            </TabsTrigger>
+          )
+        })}
+      </TabsList>
+    </header>
+  )
+}
+
+export default Header
