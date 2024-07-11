367aa1b9a1f56d2b6badae3ede421261c76c05b9
joshtin2505 <justincastro2505@gmail.com>
2024-07-10 21:07:29 -0500

    chore: Update ThemeButton component to handle theme changes and persist theme preference in local storage

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -2,16 +2,23 @@
 import { useEffect, useState } from "react"
 import { Button } from "./ui/button"
 import { IconMoon, IconSunHigh } from "@tabler/icons-react"
+import { getItem, setItem } from "@/utils/LocalStorage"
+import { ThemeType } from "@/types.d"
 
 function ThemeButton() {
-  const [theme, setTheme] = useState<"dark" | "light">("light")
+  const [theme, setTheme] = useState<ThemeType>(() => {
+    const savedTheme = getItem("theme") as ThemeType
+    return savedTheme || "light"
+  })
   const handleClick = () => setTheme(theme === "light" ? "dark" : "light")
 
   useEffect(() => {
     if (theme === "light") {
       document.documentElement.classList.remove("dark")
+      setItem("theme", "light")
     } else {
       document.documentElement.classList.add("dark")
+      setItem("theme", "dark")
     }
   }, [theme])
 
