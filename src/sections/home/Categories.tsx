e1a66d938a59d35d62d159a59c618024c71a1d29
joshtin2505 <justincastro2505@gmail.com>
2024-07-11 22:05:36 -0500

    refactor: Update Categories component to use 'value' instead of 'id' for TabsTrigger

--- src/sections/home/Categories.tsx
+++ src/sections/home/Categories.tsx
@@ -5,13 +5,17 @@ import categories from "@/mocks/section.json"
 import Image from "next/image"
 function Categories() {
   return (
-    <Tabs defaultValue={categories[0].id}>
+    <Tabs defaultValue={categories[0].value}>
       <section className="h-dvh pt-10">
         <Header categories={categories} />
         {categories.map((category) => {
-          const { id, name } = category
+          const { value, name } = category
           return (
-            <TabsContent value={id} key={id} className="pb-4 pt-2 h-[94%]">
+            <TabsContent
+              value={value}
+              key={value}
+              className="pb-4 pt-2 h-[94%]"
+            >
               {/* <h3 className="text-3xl">{name}</h3> */}
               <Image
                 className="w-full object-cover h-full aspect-auto rounded grayscale"
