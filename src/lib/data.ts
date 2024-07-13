8354dac7bdec38952ca1de4220280a44a970d49a
joshtin2505 <justincastro2505@gmail.com>
2024-07-12 23:47:55 -0500

    refactor: Update fetchCategories function to use 'imagesrc' as the property name for category image source

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,8 +1,9 @@
+"use server"
 import { CategoryType } from "@/types.d"
 import { sql } from "@vercel/postgres"
 
 export async function fetchCategories() {
   const { rows } =
-    await sql<CategoryType>`SELECT name, value, image_src FROM categories`
+    await sql<CategoryType>`SELECT name, value, image_src AS imagesrc FROM categories`
   return rows
 }
