a6854a37927760508400d51dc771c1adaecfb024
joshtin2505 <justincastro2505@gmail.com>
2024-07-12 23:35:18 -0500

    refactor: Add USER_STATUS enum to types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -6,4 +6,11 @@ interface CategoryType {
 }
 type CategoriesType = CategoryType[]
 
+export enum USER_STATUS {
+  LOGGED = "LOGGED",
+  PASSWORD_NOT_MATCH = "PASSWORD_NOT_MATCH",
+  MAIL_NOT_FOUND = "MAIL_NOT_FOUND",
+  INTERNAL_ERROR = "INTERNAL_ERROR",
+}
+
 export type { ThemeType, CategoriesType, CategoryType }
