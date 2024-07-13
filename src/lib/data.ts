0dd5bbfb8c9506ecdad2c90a6ea456bef997ee6d
joshtin2505 <justincastro2505@gmail.com>
2024-07-12 23:53:54 -0500

    .

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -3,7 +3,6 @@ import { CategoryType } from "@/types.d"
 import { sql } from "@vercel/postgres"
 
 export async function fetchCategories() {
-  const { rows } =
-    await sql<CategoryType>`SELECT name, value, image_src AS imagesrc FROM categories`
+  const { rows } = await sql<CategoryType>`SELECT name, value, image_src AS imagesrc FROM categories`
   return rows
 }
