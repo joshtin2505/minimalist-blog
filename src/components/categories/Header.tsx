5d950b80c023bfe83df75e1eddafe77a86d24de9
joshtin2505 <justincastro2505@gmail.com>
2024-07-12 23:35:45 -0500

    refactor: Update Header component to handle empty categories array

--- src/components/categories/Header.tsx
+++ src/components/categories/Header.tsx
@@ -1,7 +1,8 @@
 import { CategoriesType } from "@/types"
 import { TabsList, TabsTrigger } from "../ui/tabs"
 
-function Header({ categories }: { categories: CategoriesType }) {
+function Header({ categories = [] }: { categories: CategoriesType }) {
+  if (categories.length === 0) return null
   return (
     <header className="flex w-full justify-between ">
       <h3 className="text-3xl">Categories</h3>
