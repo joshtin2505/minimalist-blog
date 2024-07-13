29479216c75db8b6c2a139be9285d7d262160d2d
joshtin2505 <justincastro2505@gmail.com>
2024-07-12 23:37:15 -0500

    refactor: Remove unused code and optimize Categories component

--- src/sections/home/Categories.tsx
+++ src/sections/home/Categories.tsx
@@ -1,32 +1,15 @@
 import Header from "@/components/categories/Header"
-import { Tabs, TabsContent } from "@/components/ui/tabs"
+import { Tabs } from "@/components/ui/tabs"
 import React from "react"
 import categories from "@/mocks/section.json"
-import Image from "next/image"
+import Content from "@/components/categories/Content"
 function Categories() {
+  if (categories.length === 0) return null
   return (
     <Tabs defaultValue={categories[0].value}>
       <section className="h-dvh pt-10">
         <Header categories={categories} />
-        {categories.map((category) => {
-          const { value, name } = category
-          return (
-            <TabsContent
-              value={value}
-              key={value}
-              className="pb-4 pt-2 h-[94%]"
-            >
-              {/* <h3 className="text-3xl">{name}</h3> */}
-              <Image
-                className="w-full object-cover h-full aspect-auto rounded grayscale"
-                src={category.imageSrc}
-                alt={name}
-                width={2000}
-                height={2000}
-              />
-            </TabsContent>
-          )
-        })}
+        <Content categories={categories} />
       </section>
     </Tabs>
   )
