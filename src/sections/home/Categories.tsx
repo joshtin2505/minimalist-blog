8b09ac8008113d2c361bb03f59901142290cd68a
joshtin2505 <justincastro2505@gmail.com>
2024-07-12 23:48:11 -0500

    refactor: Update Categories component to fetch and display categories dynamically

--- src/sections/home/Categories.tsx
+++ src/sections/home/Categories.tsx
@@ -1,9 +1,18 @@
+"use client"
 import Header from "@/components/categories/Header"
 import { Tabs } from "@/components/ui/tabs"
-import React from "react"
-import categories from "@/mocks/section.json"
+import React, { useEffect, useState } from "react"
+// import categories from "@/mocks/section.json"
 import Content from "@/components/categories/Content"
+import { fetchCategories } from "@/lib/data"
+import { CategoriesType } from "@/types"
 function Categories() {
+  const [categories, setCategories] = useState<CategoriesType>([])
+  useEffect(() => {
+    ;(async () => {
+      setCategories(await fetchCategories())
+    })()
+  }, [])
   if (categories.length === 0) return null
   return (
     <Tabs defaultValue={categories[0].value}>
