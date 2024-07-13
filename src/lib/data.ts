81f1d3cc54ee4746d95476afbb7aba9e39c49f37
joshtin2505 <justincastro2505@gmail.com>
2024-07-12 23:37:04 -0500

    refactor: Add lazy loading for category images in fetchCategories function

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,1 +1,8 @@
+import { CategoryType } from "@/types.d"
 import { sql } from "@vercel/postgres"
+
+export async function fetchCategories() {
+  const { rows } =
+    await sql<CategoryType>`SELECT name, value, image_src FROM categories`
+  return rows
+}
