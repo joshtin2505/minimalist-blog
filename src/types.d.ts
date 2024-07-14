15939055b51ae754974127e3f0614dfd6601087c
joshtin2505 <justincastro2505@gmail.com>
2024-07-13 23:01:38 -0500

    refactor: Update types.d.ts to include LoggedUserType and ReturnLoginType

--- src/types.d.ts
+++ src/types.d.ts
@@ -6,6 +6,30 @@ interface CategoryType {
 }
 type CategoriesType = CategoryType[]
 
+type IdType = `${string}-${string}-${string}-${string}-${string}`
+type EmailType = `${string}@${string}.${string}`
+type LoggedUserType = {
+  id: IdType
+  name: string
+  lastname: string
+  email: string
+  username: string
+}
+
+type ReturnLoginType =
+  | {
+      status: USER_STATUS.LOGGED
+      user: {
+        id: IdType
+        email: EmailType
+        name: string
+        lastname: string
+        username: string
+      }
+    }
+  | {
+      status: Exclude<USER_STATUS, USER_STATUS.LOGGED>
+    }
 export enum USER_STATUS {
   LOGGED = "LOGGED",
   PASSWORD_NOT_MATCH = "PASSWORD_NOT_MATCH",
@@ -13,4 +37,10 @@ export enum USER_STATUS {
   INTERNAL_ERROR = "INTERNAL_ERROR",
 }
 
-export type { ThemeType, CategoriesType, CategoryType }
+export type {
+  ThemeType,
+  CategoriesType,
+  CategoryType,
+  LoggedUserType,
+  ReturnLoginType,
+}
