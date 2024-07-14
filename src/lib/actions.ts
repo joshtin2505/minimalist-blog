88cf8c49888590e9eb85b4c06d6fb4fd52272d33
joshtin2505 <justincastro2505@gmail.com>
2024-07-13 23:02:26 -0500

    refactor: Update loginUser function to return user object upon successful login

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,30 +1,42 @@
 "use server"
 import { LoginSchema } from "@/schemas/users"
-import { USER_STATUS } from "@/types.d"
+import useStore from "@/store/useStore"
+import { ReturnLoginType, USER_STATUS } from "@/types.d"
 import { sql } from "@vercel/postgres"
 import bcrypt from "bcrypt"
-export async function loginUser(data: LoginSchema): Promise<USER_STATUS> {
+
+export async function loginUser(data: LoginSchema): Promise<ReturnLoginType> {
   const { email, password } = data
 
   try {
     const { rows } = await sql`
-    SELECT id, email, password
+    SELECT id, email, password, name, last_name, username
     FROM users
     WHERE email = ${email}
     `
     if (rows.length === 0) {
       console.log("Email not found")
-      return USER_STATUS.MAIL_NOT_FOUND
+      return { status: USER_STATUS.MAIL_NOT_FOUND }
     }
     const isMatch = await bcrypt.compare(password, rows[0].password)
 
     if (isMatch) {
-      return USER_STATUS.LOGGED
+      console.log("User logged in")
+      return {
+        status: USER_STATUS.LOGGED,
+        user: {
+          id: rows[0].id,
+          email: rows[0].email,
+          name: rows[0].name,
+          lastname: rows[0].last_name,
+          username: rows[0].username,
+        },
+      }
     } else {
-      return USER_STATUS.PASSWORD_NOT_MATCH
+      return { status: USER_STATUS.PASSWORD_NOT_MATCH }
     }
   } catch (error) {
     console.error(error)
-    return USER_STATUS.INTERNAL_ERROR
+    return { status: USER_STATUS.INTERNAL_ERROR }
   }
 }
