5613123dec337bcf7b3a0a98e8dfc81e1c2c3354
joshtin2505 <justincastro2505@gmail.com>
2024-07-15 23:03:35 -0500

    refactor: Remove unused 'value' prop in Categories Content component

--- src/components/categories/Content.tsx
+++ src/components/categories/Content.tsx
@@ -7,13 +7,9 @@ function Content({ categories = [] }: { categories: CategoriesType }) {
     <>
       {categories.length > 0 &&
         categories.map((category) => {
-          const { value, name, imagesrc } = category
+          const { name, imagesrc } = category
           return (
-            <TabsContent
-              value={value}
-              key={value}
-              className="pb-4 pt-2 h-[94%]"
-            >
+            <TabsContent value={name} key={name} className="pb-4 pt-2 h-[94%]">
               {/* <h3 className="text-3xl">{name}</h3> */}
               <Image
                 className="w-full object-cover h-full aspect-auto rounded grayscale"
