24b13ecc8f1b03aadbac4d0923c2e73e8bc861f6
joshtin2505 <justincastro2505@gmail.com>
2024-07-16 18:57:10 -0500

    refactor: Add Logout component to dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { auth } from "@/auth"
+import Logout from "@/components/Logout"
 
 export default async function Page() {
   const session = await auth()
@@ -10,6 +11,7 @@ export default async function Page() {
   return (
     <div className="container">
       <pre>{JSON.stringify(session, null, 2)}</pre>
+      <Logout />
     </div>
   )
 }
