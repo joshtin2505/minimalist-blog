0048940ada646115a836207a0ab1286b49ef111b
joshtin2505 <justincastro2505@gmail.com>
2024-07-15 23:03:01 -0500

    refactor: Update authentication logic to use bcryptjs for password comparison

--- auth.config.ts
+++ auth.config.ts
@@ -1,28 +1,39 @@
+import { loginSchema } from "@/schemas/users"
 import type { NextAuthConfig } from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import { db } from "@/lib/db"
+import bcrypt from "bcryptjs"
 
 export default {
   providers: [
-    // Credentials({
-    //   // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-    //   // e.g. domain, username, password, 2FA token, etc.
-    //   credentials: {
-    //     email: {},
-    //     password: {},
-    //   },
-    //   authorize: async (credentials) => {
-    //     let user = null
-    //     // logic to salt and hash password
-    //     const pwHash = saltAndHashPassword(credentials.password)
-    //     // logic to verify if user exists
-    //     user = await getUserFromDb(credentials.email, pwHash)
-    //     if (!user) {
-    //       // No user found, so this is their first attempt to login
-    //       // meaning this is also the place you could do registration
-    //       throw new Error("User not found.")
-    //     }
-    //     // return user object with the their profile data
-    //     return user
-    //   },
-    // }),
+    Credentials({
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      authorize: async (credentials) => {
+        const { data, success } = loginSchema.safeParse(credentials)
+
+        if (!success) {
+          throw new Error("Invalid credentials")
+        }
+
+        const user = await db.user.findUnique({
+          where: {
+            email: data.email,
+          },
+        })
+        if (!user || !user.password) {
+          throw new Error("No user found")
+        }
+        const passwordsMatch = await bcrypt.compare(
+          data.password,
+          user.password,
+        )
+        if (!passwordsMatch) {
+          throw new Error("Password does not match")
+        }
+
+        return user
+      },
+    }),
   ],
 } satisfies NextAuthConfig
