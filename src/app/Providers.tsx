0409d1f53db0bbd26f1cd67eb253ef6f1ab239b8
joshtin2505 <justincastro2505@gmail.com>
2024-07-16 20:05:11 -0500

    refactor: Add TooltipProvider to Providers component for tooltip functionality

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,12 +1,13 @@
 "use client"
 import { ReactNode } from "react"
 import { ThemeProvider } from "next-themes"
+import { TooltipProvider } from "@/components/ui/tooltip"
 
 function Providers({ children }: { children: ReactNode }) {
   return (
     <>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        {children}
+        <TooltipProvider>{children}</TooltipProvider>
       </ThemeProvider>
     </>
   )
