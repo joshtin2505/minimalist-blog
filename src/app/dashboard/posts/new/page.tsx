76f62cd266639b81d20628a6309ae8ef8fd8a8bb
joshtin2505 <justincastro2505@gmail.com>
2024-07-20 16:23:47 -0500

    feat: Create new page component for creating posts

---
+++ src/app/dashboard/posts/new/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { JSONContent } from "novel"
+import { useState } from "react"
+function New() {
+  return (
+    <div>
+      <h1>Create New Post</h1>
+    </div>
+  )
+}
+
+export default New
