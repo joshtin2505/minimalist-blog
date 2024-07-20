d2d88ef0a097c50095ba1f9f36d93dfe3d1ed696
joshtin2505 <justincastro2505@gmail.com>
2024-07-20 16:24:04 -0500

    refactor: Remove BasicLayout from login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,9 @@
-import BasicLayout from "@/components/layouts/BasicLayout"
 import { LoginForm } from "@/components/login/LoginForm"
 import { IconWaveSine } from "@tabler/icons-react"
 import Link from "next/link"
-import React from "react"
-
 function Page() {
   return (
-    <BasicLayout className="relative flex flex-col items-center justify-center">
+    <>
       <Link
         href="/"
         className="flex absolute top-0 h-14 left-8 items-center gap-2 font-medium"
@@ -15,7 +12,7 @@ function Page() {
         <h3 className="text-lg">Minimal Blog</h3>
       </Link>
       <LoginForm />
-    </BasicLayout>
+    </>
   )
 }
 
