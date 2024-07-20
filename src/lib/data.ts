9fc2871b23181aaefd970deafa83b7f9f599ee06
joshtin2505 <justincastro2505@gmail.com>
2024-07-20 16:24:15 -0500

    refactor: Remove console.log statement from fetchCategories function

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -4,7 +4,6 @@ import { db } from "./db"
 export async function fetchCategories() {
   try {
     const rows = await db.categories.findMany()
-    console.log(rows)
     return rows
   } catch (error) {
     console.error(error)
