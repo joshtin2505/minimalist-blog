5c6800f6465944c5d4a639a5bf05dd3e1b457298
joshtin2505 <justincastro2505@gmail.com>
2024-07-20 23:44:25 -0500

    refactor: Remove commented code and simplify layout component

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,19 +1,14 @@
 "use client"
 import Aside from "@/sections/dashboard/AsideDashboard"
 import HeaderDashboard from "@/sections/dashboard/HeaderDashboard"
-// import { usePathname } from "next/navigation"
+import { usePathname } from "next/navigation"
 import React from "react"
 
 function Layout({ children }: { children: React.ReactNode }) {
-  // const pathname = usePathname()
-  // if (pathname.includes("/new")) {
-  //   return (
-  //     <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-  //       <HeaderDashboard />
-  //       {children}
-  //     </div>
-  //   )
-  // }
+  const pathname = usePathname()
+  if (pathname.includes("/new")) {
+    return <div className="">{children}</div>
+  }
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <Aside />
