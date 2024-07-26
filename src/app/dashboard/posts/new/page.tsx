0ad0d05eab34ec4b5c1a89b877037f256b47845a
joshtin2505 <justincastro2505@gmail.com>
2024-07-25 22:42:29 -0500

    refactor: Update page component to include category selection and text editor functionality

--- src/app/dashboard/posts/new/page.tsx
+++ src/app/dashboard/posts/new/page.tsx
@@ -1,7 +1,75 @@
 "use client"
-import TailwindAdvancedEditor from "@/components/text-editor/advanced-editor"
+
+import { getCategories } from "@/actions/categories"
+import TextEditor from "@/components/TextEditor"
+import { Button } from "@/components/ui/button"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { CategoryPureType } from "@/types"
+import { X } from "lucide-react"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import { toast } from "sonner"
+
+// import TailwindAdvancedEditor from "@/components/text-editor/advanced-editor"
 function New() {
-  return <TailwindAdvancedEditor />
+  const [categories, setCategories] = useState<CategoryPureType[]>([])
+  useEffect(() => {
+    ;(async () => {
+      await getCategories()
+        .then((res) => setCategories(res))
+        .catch((error) => {
+          toast.error(error)
+          console.log(error)
+          setCategories([])
+        })
+    })()
+  }, [])
+  return (
+    <div className="pt-4">
+      <header className="flex justify-end w-full px-4 gap-4 items-center">
+        <Select>
+          {categories.length > 0 ? (
+            <>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Select a category" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Category</SelectLabel>
+                  {categories.map((category) => (
+                    <>
+                      <SelectItem key={category.id} value={category.id}>
+                        {category.name}
+                      </SelectItem>
+                    </>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </>
+          ) : (
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="No categories found" />
+            </SelectTrigger>
+          )}
+        </Select>
+        <Button variant="outline">Publicar</Button>
+        <Button variant="default">Guardar</Button>
+        <Link href="/dashboard/posts">
+          <X size={24} className="cursor-pointer ml-5" />
+        </Link>
+      </header>
+      <div className=""></div>
+      <TextEditor />
+    </div>
+  )
 }
 
 export default New
