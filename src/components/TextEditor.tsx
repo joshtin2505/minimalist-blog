b46dc77630cc2c8d2a3ef0f0bb65a34cc88a6bff
joshtin2505 <justincastro2505@gmail.com>
2024-07-28 16:36:27 -0500

    refactor: Update TextEditor component to use @tiptap/react instead of useEditor hook

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,25 +1,9 @@
 "use client"
 import React from "react"
 import extensions from "@/lib/editor"
-import { EditorContent, useEditor } from "@tiptap/react"
+import { Editor, EditorContent } from "@tiptap/react"
 
-function TextEditor() {
-  const editor = useEditor({
-    extensions,
-    editable: true,
-    autofocus: true,
-    onUpdate: ({ editor }) => {
-      const doc = editor.state.doc
-
-      // Verificar si la primera línea es un encabezado
-      if (!doc.firstChild || doc.firstChild.type.name !== "heading") {
-        editor.chain().focus().setNode("heading", { level: 1 }).run()
-      }
-    },
-    content: "<h1>New Post</h1>",
-    injectCSS: true,
-  })
-  if (!editor) return null
+function TextEditor({ editor }: { editor: Editor }) {
   return <EditorContent className="w-full" editor={editor} />
 }
 
