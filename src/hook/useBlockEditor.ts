c2e4973ec6f4956804ac0a8b97f077188e94e0fc
joshtin2505 <justincastro2505@gmail.com>
2024-08-02 19:30:57 -0500

    feat: Add useBlockEditor hook for collaborative editing functionality

---
+++ src/hook/useBlockEditor.ts
@@ -0,0 +1,105 @@
+"use client"
+import { useEffect, useMemo, useState } from "react"
+
+import { Editor, useEditor } from "@tiptap/react"
+import Collaboration from "@tiptap/extension-collaboration"
+import CollaborationCursor from "@tiptap/extension-collaboration-cursor"
+import { TiptapCollabProvider, WebSocketStatus } from "@hocuspocus/provider"
+import type { Doc as YDoc } from "yjs"
+
+import { userColors, userNames } from "../lib/constants"
+import { randomElement } from "../lib/utils"
+import { EditorUser } from "../components/text-editor/types"
+import { useSidebar } from "./useSidebar"
+// import { initialContent } from '@/lib/data/initialContent'
+import ExtensionKit from "@/lib/utils/extension-kit"
+
+declare global {
+  interface Window {
+    editor: Editor | null
+  }
+}
+
+export const useBlockEditor = ({
+  ydoc,
+  provider,
+}: {
+  ydoc: YDoc
+  provider?: TiptapCollabProvider | null | undefined
+}) => {
+  const leftSidebar = useSidebar()
+  const [collabState, setCollabState] = useState<WebSocketStatus>(
+    WebSocketStatus.Connecting,
+  )
+  const editor = useEditor(
+    {
+      autofocus: true,
+      onCreate: ({ editor }) => {
+        provider?.on("synced", () => {
+          // if (editor.isEmpty) {
+          //   editor.commands.setContent(initialContent)
+          // }
+        })
+      },
+      extensions: [
+        ...ExtensionKit({
+          provider,
+        }),
+        Collaboration.configure({
+          document: ydoc,
+        }),
+        CollaborationCursor.configure({
+          provider,
+          user: {
+            name: randomElement(userNames),
+            color: randomElement(userColors),
+          },
+        }),
+      ],
+      editorProps: {
+        attributes: {
+          autocomplete: "off",
+          autocorrect: "off",
+          autocapitalize: "off",
+          class: "min-h-full",
+        },
+      },
+      immediatelyRender: false,
+    },
+    [ydoc, provider],
+  )
+
+  const users = useMemo(() => {
+    if (!editor?.storage.collaborationCursor?.users) {
+      return []
+    }
+
+    return editor.storage.collaborationCursor?.users.map((user: EditorUser) => {
+      const names = user.name?.split(" ")
+      const firstName = names?.[0]
+      const lastName = names?.[names.length - 1]
+      const initials = `${firstName?.[0] || "?"}${lastName?.[0] || "?"}`
+
+      return { ...user, initials: initials.length ? initials : "?" }
+    })
+  }, [editor?.storage.collaborationCursor?.users])
+
+  const characterCount = editor?.storage.characterCount || {
+    characters: () => 0,
+    words: () => 0,
+  }
+
+  useEffect(() => {
+    provider?.on("status", (event: { status: WebSocketStatus }) => {
+      setCollabState(event.status)
+    })
+  }, [provider])
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.editor = editor
+    }
+  }, [editor])
+
+  return { editor, users, characterCount, collabState, leftSidebar }
+}
